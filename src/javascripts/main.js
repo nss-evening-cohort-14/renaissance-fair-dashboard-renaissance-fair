e7381ac2cd8068c503da339945c5943581da4b11
Jesse Robinson <45837967+jrobinson0529@users.noreply.github.com>
2021-03-15 19:01:55 -0500

    added dombuilder for setup

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,14 +1,10 @@
 // USE WITH FIREBASE AUTH
-// import checkLoginStatus from './helpers/auth';
-
+import checkLoginStatus from './helpers/auth';
 import '../styles/main.scss';
 
 const init = () => {
-  document.querySelector('#app').innerHTML = '<h1>HELLO! You are up and running!</h1>';
-  console.warn('YOU ARE UP AND RUNNING!');
-
   // USE WITH FIREBASE AUTH
-  // checkLoginStatus();
+  checkLoginStatus();
 };
 
 init();
