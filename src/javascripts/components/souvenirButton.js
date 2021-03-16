a68b9034b11a3375d87b6919af237107b804d08f
Jesse Robinson <45837967+jrobinson0529@users.noreply.github.com>
2021-03-16 09:57:25 -0500

    fixed 404 errors in console

--- src/javascripts/components/souvenirButton.js
+++ src/javascripts/components/souvenirButton.js
@@ -1,6 +1,5 @@
 const waresButton = () => `<div id="souvenir-container">
                         <div class="card wares-button-card" style="width: 18rem;">
-                          <img src="..." class="card-img-top top" alt="...">
                         <div class="card-body middle">
                           <h5 class="card-title">Wares</h5>
                           <p class="card-text">Something about gold and jewels and dwarves</p>
