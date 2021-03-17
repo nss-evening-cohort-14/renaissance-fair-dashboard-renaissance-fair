681100f7b3fabca5007f3cd470b05294abf00cff
Jesse Robinson <45837967+jrobinson0529@users.noreply.github.com>
2021-03-17 17:42:27 -0500

    minor styling tweaks

--- src/javascripts/components/staffButton.js
+++ src/javascripts/components/staffButton.js
@@ -1,5 +1,5 @@
 const staffButton = () => `<div id="staff-container" class="nav-cards-left">
-                        <div class="card staff-button-card" style="width: 18rem;">
+                        <div class="card staff-button-card button-card" style="width: 18rem;">
                         <div class="card-body middle">
                           <h5 class="card-title">Staff Card</h5>
                           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
