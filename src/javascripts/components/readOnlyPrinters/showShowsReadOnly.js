8974b211f644c7ef01c811b765f714578d21c154
Jesse Robinson <45837967+jrobinson0529@users.noreply.github.com>
2021-03-18 13:33:08 -0500

    Created a different view for logged out users

---
+++ src/javascripts/components/readOnlyPrinters/showShowsReadOnly.js
@@ -0,0 +1,20 @@
+import headerTitle from '../headerTitle';
+
+const printShowsReadOnly = (array) => {
+  headerTitle('Watch a Show');
+  document.querySelector('#content-container').innerHTML = '';
+  array.forEach((show) => {
+    const domString = `
+    <div class="card" id="shows-card--${show.firebaseKey}" style="width: 18rem;">
+  <img src="${show.image}" class="card-img-top" alt="show image">
+  <div class="card-body-middle">
+    <h5 class="card-title">${show.name}</h5>
+    <h6 id="card-date">${show.date}</h6>
+    <p>${show.description}</p>
+  </div>
+</div>`;
+    document.querySelector('#content-container').innerHTML += domString;
+  });
+};
+
+export default printShowsReadOnly;
--- src/javascripts/helpers/startApp.js
+++ src/javascripts/helpers/startApp.js
@@ -1,23 +1,28 @@
 import buildNav from '../components/navBuilder';
 import domBuilder from './views/domBuilder';
 import headerTitle from '../components/headerTitle';
-import domEvents from './domEvents';
+import domEvents from '../events/domEvents';
 import navEvents from '../events/navEvents';
 import buttonBuilder from '../components/buttonBuilder';
 import getEvents from './data/eventsData';
 import footerBuilder from '../components/footerBuilder';
 import logoutButton from '../components/buttons/logoutButton';
+import loginButton from '../components/buttons/loginButton';
 
-const startApp = () => {
+const startApp = (user) => {
   getEvents().then((response) => {
-    domEvents(response.firebaseKey);
+    domEvents(response.firebaseKey, user);
     domBuilder();
     buildNav();
-    navEvents(response.firebaseKey);
+    navEvents(response.firebaseKey, user);
     buttonBuilder();
     headerTitle('Welcome to ye ol\' site');
     footerBuilder();
-    logoutButton();
+    if (user) {
+      logoutButton();
+    } else {
+      loginButton();
+    }
   });
 };
 
