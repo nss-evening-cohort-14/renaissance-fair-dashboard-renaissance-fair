bc4b8905ac3c19a8d4025714879fb0487063e2b7
Jesse Robinson <45837967+jrobinson0529@users.noreply.github.com>
2021-03-18 13:49:10 -0500

    fixed footer styling

--- src/javascripts/components/footerBuilder.js
+++ src/javascripts/components/footerBuilder.js
@@ -1,7 +1,7 @@
 const footerBuilder = () => {
   document.querySelector('#footer').innerHTML = `
   <footer class="navbar navbar-expand-lg footer">
-  <div class="container-fluid">
+  <div class="container-fluid d-flex justify-content-between">
     <button
       class="navbar-toggler"
       type="button"
@@ -13,31 +13,35 @@ const footerBuilder = () => {
     >
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
+    <div class="collapse navbar-collapse" id="footer">
+      <ul class="navbar-nav d-flex justify-content-around w-100">
         <li class="nav-item">
           <a class="nav-link" aria-current="page" href="#"
             >© 2021 Freelancer, LLC.</a
           >
         </li>
-        <li class="nav-item">
-          <a class="nav-link" target="_blank" href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" target="_blank" href="https://www.facebook.com/"><i class="fab fa-facebook-f"></i></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" target="_blank" href="https://twitter.com/?lang=en"><i class="fab fa-twitter"></i></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">About</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Policy</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Legal</a>
-        </li>
+        <div class="middle-footer-button-container d-flex">
+          <li class="nav-item">
+            <a class="nav-link" target="_blank" href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" target="_blank" href="https://www.facebook.com/"><i class="fab fa-facebook-f"></i></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" target="_blank" href="https://twitter.com/?lang=en"><i class="fab fa-twitter"></i></a>
+          </li>
+        </div>
+        <div class="right-footer-button-container d-flex">
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Policy</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Legal</a>
+          </li>
+        </div>
       </ul>
     </div>
   </div>
