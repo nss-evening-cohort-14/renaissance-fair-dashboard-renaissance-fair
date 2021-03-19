a039af1d05e616a1bf22da586c477aa9c38d3212
Holly Parsons <67122062+hmparsons-sde@users.noreply.github.com>
2021-03-19 16:18:46 -0500

    sidenav

--- src/javascripts/components/navBuilder.js
+++ src/javascripts/components/navBuilder.js
@@ -1,13 +1,15 @@
 const buildNav = () => {
   document.querySelector('#nav').innerHTML = `
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <nav class="navbar navbar-expand-lg menu" tabindex="0">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#" id="home">Freelancer</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavigation" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
     <div class="collapse navbar-collapse sidenav" id="navbarNavigation">
       <ul class="navbar-nav">
+      <li class="nav-item">
+      <a class="navbar-brand" href="#" id="home">Freelancer</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavigation" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      </li>
         <li class="nav-item">
           <a class="nav-link active" id="shows-link" aria-current="page" href="#">Shows</a>
         </li>
@@ -20,8 +22,10 @@ const buildNav = () => {
         <li class="nav-item">
           <a class="nav-link" id="food-link" href="#">Food Shoppes</a>
         </li>
+        <li class="nav-item">
+        <div id="auth-button"></div>
+        </li>
       </ul>
-      <div id="auth-button"></div>
     </div>
   </div>
 </nav>`;
