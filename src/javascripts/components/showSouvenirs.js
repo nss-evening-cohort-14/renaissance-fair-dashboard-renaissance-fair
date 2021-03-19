aa082ac72a196d9d3926584031e344189b312e09
Holly Parsons <67122062+hmparsons-sde@users.noreply.github.com>
2021-03-19 15:43:31 -0500

    white space fix

--- src/javascripts/components/showSouvenirs.js
+++ src/javascripts/components/showSouvenirs.js
@@ -1,7 +1,7 @@
 const showSouvenirs = (array) => {
   document.querySelector('#content-container').innerHTML = '<button class="btn btn-lg" id="add-souvenir-btn">Add A Souvenir</button>';
   array.forEach((item) => {
-    document.querySelector('#content-container').innerHTML += `<div class="item card m-3">
+    document.querySelector('#content-container').innerHTML += `<div class="item card border-0 bg-transparent">
     <div class="souvenir-container m-3" style="width: 20rem">
       <img src='${item.souvenir_image}' class="card-img-top rounded mx-auto d-block image top" id='card-img'></img>
       <div class="middle">
