1e5f7b80b71005f11a4e386f34627e6bea3b7a8a
Holly Parsons <67122062+hmparsons-sde@users.noreply.github.com>
2021-03-18 22:23:51 -0500

    first round of styling

--- src/javascripts/components/souvenirButton.js
+++ src/javascripts/components/souvenirButton.js
@@ -1,9 +1,8 @@
 const souvenirsButton = () => `<div id="souvenir-container" class="nav-cards">
                         <div class="card wares-button-card button-card" style="width: 18rem;">
+                        <img src="src/assets/gandalfpipe.jpg" alt"Blue Marbled Pipe" class="image card-img-top rounded mx-auto d-block top" style="width:100%;">
                         <div class="card-body middle">
-                          <h5 class="card-title">Souvenirs</h5>
-                          <p class="card-text">Something about gold and jewels and dwarves</p>
-                          <a href="#" class="btn btn-primary" id="souvenir-view">Souvenirs</a>
+                          <a href="#" class="btn" id="souvenir-view">Souvenirs</a>
                         </div>
                       </div>
                     </div>`;
