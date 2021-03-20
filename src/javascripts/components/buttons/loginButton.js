5cd998d9faff8fb4b7aca8ef6349579f5b7aa26a
Holly Parsons <67122062+hmparsons-sde@users.noreply.github.com>
2021-03-19 19:18:35 -0500

    fixed logo

--- src/javascripts/components/buttons/loginButton.js
+++ src/javascripts/components/buttons/loginButton.js
@@ -7,7 +7,7 @@ const signMeIn = () => {
 };
 
 const loginButton = () => {
-  const domString = '<button id="google-auth" class="btn btn-danger">GOOGLE LOGIN</button>';
+  const domString = '<button id="google-auth" class="btn btn-transparent">WELCOME</button>';
   $('#auth-button').html(domString);
   $('#google-auth').click(signMeIn);
 };
