803728ffe28fa7aac98ad932d840a525015179fc
Chie Stroud <urirunrunkoko@gmail.com>
2021-03-20 12:08:57 -0500

    adding video

--- src/javascripts/components/headerTitle.js
+++ src/javascripts/components/headerTitle.js
@@ -1,8 +1,10 @@
+import animate from '../../assets/resized-animate.mp4';
+
 const headerTitle = () => {
   const domString = `<div class="jumbotron text-center">
   <div id="animate-container">
     <video width="1000" height="400" autoplay muted>
-    <source src="src/assets/resized-animate.mp4" type="video/mp4">
+    <source src="${animate}" type="video/mp4">
     </video>
   </div>
 </div>`;
