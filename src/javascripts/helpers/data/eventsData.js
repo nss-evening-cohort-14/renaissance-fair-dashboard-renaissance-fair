e63002c5dea0b61d66c1d51a18e0679dadfc118d
Greenfin17 <jmaple@yahoo.com>
2021-04-12 19:28:00 -0500

    adding getAllEvents

--- src/javascripts/helpers/data/eventsData.js
+++ src/javascripts/helpers/data/eventsData.js
@@ -3,6 +3,11 @@ import firebaseConfig from '../apiKeys';
 
 const dbUrl = firebaseConfig.databaseURL;
 
+const getAllEvents = () => new Promise((resolve, reject) => {
+  axios.get(`${dbUrl}/dashboard.json`)
+    .then((response) => resolve(Object.values(response.data)[0]))
+    .catch((error) => reject(error));
+
 const getEvents = () => new Promise((resolve, reject) => {
   axios.get(`${dbUrl}/dashboard.json`)
     .then((response) => resolve(Object.values(response.data)[0]))
