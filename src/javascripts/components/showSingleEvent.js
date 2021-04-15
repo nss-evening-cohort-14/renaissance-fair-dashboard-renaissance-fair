f0983c7f0a47b4340f4ef2436ffaacfb7ad8dcdf
Lindsey Satterfield <satterfield.lindsey@gmail.com>
2021-04-14 22:13:04 -0500

    conflicts

--- src/javascripts/components/showSingleEvent.js
+++ src/javascripts/components/showSingleEvent.js
@@ -1,12 +1,7 @@
 const showSingleEvent = () => {
   document.querySelector('#content-container').innerHTML = '';
   document.querySelector('#add-button-container').innerHTML = '';
-  document.querySelector('#header-title-container').innerHTML = '';
   document.querySelector('#content-container').innerHTML = '<div class="event-quadrant">Food</div><div class="event-quadrant">Staff</div><div class="event-quadrant">Shows</div><div class="event-quadrant">Souvenirs</div>';
-  // array.forEach((item) => {
-  //   document.querySelector('#content-container').innerHTML += `
-  //   `;
-  // });
 };
 
 export default showSingleEvent;
